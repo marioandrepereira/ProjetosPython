23a2ac3e888280033c72293b652b5cf38bc4bfeb
Marioandre <50428621+marioandrepereira@users.noreply.github.com>
2023-01-30 12:03:22 -0300

    Add files via upload

---
+++ ssh.py
@@ -0,0 +1,34 @@
+import paramiko
+import cryptocode
+import random
+
+address_list = ["10.98.0.50", "10.98.0.30"] # Add all the host addresses in the list
+user = "suporte"
+comand = "df -h"
+porta = 12001
+
+try:
+    for address in address_list:
+        #encrypts the entered password
+        mensagem = input("Escreva a senha para acessar o host: ")
+        chave = str(random.randint(123338233093093, 1023338233093093))
+        MensagemCriptografada = cryptocode.encrypt(mensagem, chave)
+        # create an SSH client
+        client = paramiko.SSHClient()
+        # automatically add the host key 
+        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        # connect to the Host
+        client.connect(hostname=address, port=porta, username=user, password=cryptocode.decrypt(MensagemCriptografada, chave))
+
+        # execute a command 
+        stdin, stdout, stderr = client.exec_command(comand)
+
+        # print the output of the command
+        comandresul = stdout.readlines()
+        for lin in comandresul:
+            print(lin.replace("\n", ""))
+
+        # close the SSH connection
+        client.close()
+except:
+    print('Não foi possivel acessar o host!!')
